858dd7303faf2b5fe45531bbc1c5442787a98928
maxxum12@gmail.com <maxxum12@gmail.com>
2021-03-11 15:50:19 +0100

    All

--- API/Frontend_Project/Pages/Haandvaerker/Index.cshtml.cs
+++ API/Frontend_Project/Pages/Haandvaerker/Index.cshtml.cs
@@ -25,54 +25,54 @@ namespace Frontend_Project.Pages.Haandvaerker
 
         public async Task OnGet()
         {
-            var dd = new DateTime(2020, 09, 12);
-            var d1 = new DateTime(2020, 09, 11);
-            HaandvaerkerModel m1 = new HaandvaerkerModel()
-            {
-                Ansættelsesdato = dd,
-                Fornavn = "Mark",
-                Efternavn = "Hansen",
-                ID = 1234,
-                Fagområde = "Igeneiør"
-            };
-            HaandvaerkerModel m2 = new HaandvaerkerModel()
-            {
-                Ansættelsesdato = d1,
-                Fornavn = "Dres",
-                Efternavn = "Elgaard",
-                ID = 1235,
-                Fagområde = "Maler"
-            };
-            HaandvaerkerModel m3 = new HaandvaerkerModel()
-            {
-                Ansættelsesdato = dd,
-                Fornavn = "Mads",
-                Efternavn = "Jørgenjørgensen",
-                ID = 1236,
-                Fagområde = "Toiletrenser"
-            };
-            LocalModels.Add(m1);
-            LocalModels.Add(m2);
-            LocalModels.Add(m3);
-
-            //using (var client = new HttpClient())
+            //var dd = new DateTime(2020, 09, 12);
+            //var d1 = new DateTime(2020, 09, 11);
+            //HaandvaerkerModel m1 = new HaandvaerkerModel()
             //{
-            //    client.BaseAddress = new Uri("http://localhost:3000");
+            //    Ansættelsesdato = dd,
+            //    Fornavn = "Mark",
+            //    Efternavn = "Hansen",
+            //    ID = 1234,
+            //    Fagområde = "Igeneiør"
+            //};
+            //HaandvaerkerModel m2 = new HaandvaerkerModel()
+            //{
+            //    Ansættelsesdato = d1,
+            //    Fornavn = "Dres",
+            //    Efternavn = "Elgaard",
+            //    ID = 1235,
+            //    Fagområde = "Maler"
+            //};
+            //HaandvaerkerModel m3 = new HaandvaerkerModel()
+            //{
+            //    Ansættelsesdato = dd,
+            //    Fornavn = "Mads",
+            //    Efternavn = "Jørgenjørgensen",
+            //    ID = 1236,
+            //    Fagområde = "Toiletrenser"
+            //};
+            //LocalModels.Add(m1);
+            //LocalModels.Add(m2);
+            //LocalModels.Add(m3);
 
-            //    var response = client.GetAsync("/Haandvaerker");
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:3000");
 
-            //    if (response.Result.StatusCode != HttpStatusCode.OK)
-            //    {
+                var response = client.GetAsync("/Haandvaerker");
 
-            //    }
+                if (response.Result.StatusCode != HttpStatusCode.OK)
+                {
 
-            //    var json = await response.Result.Content.ReadAsStringAsync();
+                }
 
-            //    var result = JsonSerializer.Deserialize<List<HaandvaerkerModel>>(json);
+                var json = await response.Result.Content.ReadAsStringAsync();
 
-            //    LocalModels = result;
+                var result = JsonSerializer.Deserialize<List<HaandvaerkerModel>>(json);
 
-            //}
+                LocalModels = result;
+
+            }
         }
 
     }
